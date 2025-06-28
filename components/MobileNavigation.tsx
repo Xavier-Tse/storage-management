afc2b27c14b1ffa921dc2a908388043e5b25e456
XavierTse <xaviertse1024@gmail.com>
2025-06-28 14:00:45 +0800

    feat: upload files

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -4,8 +4,6 @@ import React, { useState } from 'react'
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
@@ -20,7 +18,7 @@ import FileUploader from './FileUploader'
 import { signOutUser } from '@/lib/actions/user.actions'
 
 interface Props {
-  ownerId: string
+  $id: string
   accountId: string
   fullName: string
   avatar: string
@@ -28,7 +26,7 @@ interface Props {
 }
 
 const MobileNavigation = ({
-  ownerId,
+  $id: ownerId,
   accountId,
   fullName,
   avatar,
@@ -100,7 +98,7 @@ const MobileNavigation = ({
           </nav>
           <Separator className='my-5 bg-light-200/20'/>
           <div className='flex flex-col justify-between gap-5 pb-5'>
-            <FileUploader />
+            <FileUploader ownerId={ownerId} accountId={accountId} />
             <Button
               type='submit'
               className='mobile-sign-out-button'

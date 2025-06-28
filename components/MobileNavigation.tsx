6f0dbdeed28e893864e3549a15f873a8fa15cdaf
XavierTse <xaviertse1024@gmail.com>
2025-06-28 08:40:45 +0800

    feat: finalize auth

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -17,6 +17,7 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { Button } from './ui/button'
 import FileUploader from './FileUploader'
+import { signOutUser } from '@/lib/actions/user.actions'
 
 interface Props {
   ownerId: string
@@ -100,7 +101,11 @@ const MobileNavigation = ({
           <Separator className='my-5 bg-light-200/20'/>
           <div className='flex flex-col justify-between gap-5 pb-5'>
             <FileUploader />
-            <Button type='submit' className='mobile-sign-out-button' onClick={() => {}}>
+            <Button
+              type='submit'
+              className='mobile-sign-out-button'
+              onClick={async () => await signOutUser()}
+            >
               <Image
                 src='/assets/icons/logout.svg'
                 alt='logo'

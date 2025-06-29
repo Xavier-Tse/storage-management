fd6f50c8a65b5536158fbbad71a5850d69671296
XavierTse <xaviertse1024@gmail.com>
2025-06-29 18:05:59 +0800

    feat: share file

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -17,7 +17,7 @@ import Link from 'next/link'
 import { constructDownloadUrl } from '@/lib/utils'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
-import { renameFile, updateFileUsers } from '@/lib/actions/file.actions'
+import { deleteFile, renameFile, updateFileUsers } from '@/lib/actions/file.actions'
 import { usePathname } from 'next/navigation'
 import { FileDetails, ShareInput } from './ActionModalContent'
 
@@ -52,7 +52,7 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
         path,
       }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log('delete'),
+      delete: () => deleteFile({ fileId: file.$id, path, bucketFileId: file.bucketFileId }),
     }
     success = await actions[action.value as keyof typeof actions]()
     if (success) {
@@ -104,6 +104,12 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
           )}
           {value === 'details' && (
             <FileDetails file={file} />
+          )}
+          {value === 'delete' && (
+            <p className='delete-confirmation'>
+              Are you sure you want to delete{` `}
+              <span className='delete-file-name'>{file.name}</span>
+            </p>
           )}
         </DialogHeader>
         {['rename', 'delete', 'share'].includes(value) && (
